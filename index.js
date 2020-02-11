5e0904c8c5700e92e589172ebc1140814c5715e6
Sarah Armitage <saraharmitage314@gmail.com>
2020-02-11 09:47:29 -0500

    test

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const pino = require('express-pino-logger')();
 const app = express()
 const session = require('express-session');
 
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
@@ -171,13 +179,7 @@ app.listen(3001, (err) => {
 //     console.log(`Mixing it up on port ${PORT}`)
 // })
 
-const path = require('path')
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
-// Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+
 
 // last resorts
 process.on('uncaughtException', (err) => {
