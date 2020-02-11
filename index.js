350044204f4139e3a6306ccff274afd2bd7fd2c1
Sarah Armitage <saraharmitage314@gmail.com>
2020-02-11 10:26:08 -0500

    test2

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var sess;
 // https://www.postgresql.org/docs/9.6/static/libpq-envars.html
 // put in the .env file later
 const HOST = 'work-samples-db.cx4wctygygyq.us-east-1.rds.amazonaws.com'
-// const PORT = 5432
+const PORT = 5432
 const DATABASE = 'work_samples'
 const USER = 'readonly'
 const PASSWORD = 'w2UIO@#bg532!'
@@ -166,18 +166,27 @@ app.listen(3001, (err) => {
 })
 
 // Choose the port and start the server
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-    console.log(`Mixing it up on port ${PORT}`)
-})
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT, () => {
+//     console.log(`Mixing it up on port ${PORT}`)
+// })
 
 const path = require('path')
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
-// Anything that doesn't match the above, send back index.html
+// app.use(express.static(path.join(__dirname, 'client/build')))
+// // Anything that doesn't match the above, send back index.html
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
+
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    // app.use(express.static('client/build'));
+}
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 // last resorts
 process.on('uncaughtException', (err) => {
